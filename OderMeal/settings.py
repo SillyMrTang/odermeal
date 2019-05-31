c1f1cb4bb4a20dec990283823b49350000cb3afe
sillmrtang <653411722@qq.com>
2019-05-31 10:24:48 +0800

    add crontab

--- OderMeal/settings.py
+++ OderMeal/settings.py
@@ -132,10 +132,12 @@ APPID = "wxf49edacad8eb4029"
 AppSECRET = "b8b72122f2a2b85ff7acc4c6a9251326"
 # APPEND_SLASH = False
 CRONJOBS = [
-    ('05 6 * * *', 'meal.core.task', '>> /home/crontab.log'),
-    ('20 10 * * *', 'meal.core.task', '>> /home/crontab.log'),
+    ('0 6 * * *', 'meal.core.task', '>> /home/crontab.log'),
+    ('0 10 * * *', 'meal.core.task', '>> /home/crontab.log'),
     ('45 15 * * *', 'meal.core.task', '>> /home/crontab.log'),
-    ('55 17 * * *', 'meal.core.task_two', '>> /home/crontab.log')
+    ('0 18 * * *', 'meal.core.task_two', '>> /home/crontab.log'),
+    ('0 8 * * *', 'meal.core.task_two', '>> /home/crontab.log'),
+    ('0 12 * * *', 'meal.core.task_two', '>> /homs/crontab.log')
 ]
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
